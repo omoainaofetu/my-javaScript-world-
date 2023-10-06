d54b3aa4f2cfc2d6ef4293199e57b5115b54f341
omoainaofetu <you@example.com>
2023-10-06 10:15:03 +0100

    my javascript world

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// 1. Given a string 'str' write program to check if string contains "script"
+
+let str_1 = "we can write a script to automate stuffs"
+let str_2 = "Promming langs include: 'java', 'python', 'javascript', and 'Go'"
+let str_3 = "we'll all have a swell weekend of coding!"
+
+if (str_1.includes("script")) {
+  console.log ("yes, I can find script");
+} 
+else {
+    console.log ("No!, I can't find script");        
+}
+if (str_2.includes("script")) {
+  console.log ("yes, I can find script");
+} 
+else {
+    console.log ("No!, I can't find script");        
+}
+
+if (str_3.includes("script")) {
+   console.log ("yes, I can find script");
+} 
+else {
+    console.log ("No!, I can't find script");        
+}
+
+// 2. Given a string name write a program to check if this string is big or not. a string is big if length of string ios over 10 characters
+// And print output true or fasle
+
+
+if ( str_1.length > 10){
+    console.log(True);
+}
+else{
+    consle.log(False);
+}
+
+
+
